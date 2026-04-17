b8dcb630e267031dc1b7b532c08858b222e79117
miguelhortaaa <miguelhorta0004@gmail.com>
2026-04-17 23:28:35 +0100

    feito
    feito

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,3 +1,11 @@
+/*
+ITW 25/26 Grupo 50
+
+David Coelho 65929 - PL 23
+Miguel Horta 62208 - PL 23
+Rodrigo Duarte 60354 - PL 23
+*/
+
 (function () {
   var audio = document.querySelector('.bg-music');
   var btn = document.querySelector('.music-toggle');
@@ -17,4 +25,24 @@
       icon.src = 'Media/mute.png';
     }
   });
+})();
+
+(function () {
+  var audio = document.querySelector('.bg-music');
+  var slider = document.getElementById('volume-slider');
+  if (!audio || !slider) return;
+  var k = 'volumeMusicaBejeweled';
+  var g = localStorage.getItem(k);
+  if (g !== null) {
+    var n = parseInt(g, 10);
+    if (!isNaN(n) && n >= 0 && n <= 100) {
+      slider.value = n;
+      audio.volume = n / 100;
+    }
+  }
+  slider.oninput = function () {
+    var p = parseFloat(slider.value);
+    localStorage.setItem(k, String(Math.round(p)));
+    audio.volume = p / 100;
+  };
 })();
